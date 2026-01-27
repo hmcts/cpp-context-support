1c1b338dd2127218bb56b886dc1f31a1fc8847cb
MohamedFarouk-HMCTS <Mohamed.Farouk@HMCTS.NET>
2026-01-27 11:47:04 +0000

    PEG-2848: Upgrade to framework D (#6)

--- support-integration-test/src/test/java/uk/gov/moj/cpp/support/integration/test/SendFeedbackIT.java
+++ support-integration-test/src/test/java/uk/gov/moj/cpp/support/integration/test/SendFeedbackIT.java
@@ -5,7 +5,7 @@ import static java.lang.String.format;
 import static java.time.ZoneOffset.UTC;
 import static java.time.ZonedDateTime.now;
 import static java.util.UUID.randomUUID;
-import static javax.json.Json.createObjectBuilder;
+import static uk.gov.justice.services.messaging.JsonObjects.createObjectBuilder;
 import static javax.ws.rs.core.Response.Status.ACCEPTED;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.CoreMatchers.is;
