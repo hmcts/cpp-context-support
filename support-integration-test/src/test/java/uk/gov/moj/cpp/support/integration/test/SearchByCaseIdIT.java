3eedeb3341e4d4473274ac50b4908a6f9c37fa8e
Oktay Ekincioglu <34853883+oktayekincioglu@users.noreply.github.com>
2026-02-10 16:27:41 +0000

    Framework E - Public Beta AppEng owned services - Release 1 (#8)

--- support-integration-test/src/test/java/uk/gov/moj/cpp/support/integration/test/SearchByCaseIdIT.java
+++ support-integration-test/src/test/java/uk/gov/moj/cpp/support/integration/test/SearchByCaseIdIT.java
@@ -3,7 +3,7 @@ package uk.gov.moj.cpp.support.integration.test;
 import static com.google.common.collect.ImmutableMap.of;
 import static java.lang.String.format;
 import static java.util.UUID.randomUUID;
-import static javax.json.Json.createObjectBuilder;
+import static uk.gov.justice.services.messaging.JsonObjects.createObjectBuilder;
 import static javax.ws.rs.core.Response.Status.ACCEPTED;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.CoreMatchers.is;
@@ -70,6 +70,7 @@ public class SearchByCaseIdIT {
     @BeforeEach
     public void cleanEventLogTable() {
         databaseCleaner.cleanEventLogTable(CONTEXT_NAME);
+        databaseCleaner.cleanProcessedEventTable(CONTEXT_NAME);
         databaseCleaner.cleanViewStoreTables(CONTEXT_NAME, "feedback");
     }
 
